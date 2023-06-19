20b86f83a2144196375b33f5e35ad29d20aea23e
alicia22nzd <alicia22nzd@gmail.com>
2023-06-19 14:19:41 +0300

    init c# intro to grpc

---
+++ 1-Interconnecting-individual-nodes/cSharp/grpcintro-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grpcintro_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- PersonalProjects/InternetSpeed/cSharp/GRPC-LatencyChecker/GRPC-LatencyChecker/Program.cs
+++ PersonalProjects/InternetSpeed/cSharp/GRPC-LatencyChecker/GRPC-LatencyChecker/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using Grpc.Net.Client;
+using System.Collections.Generic;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using var channel = GrpcChannel.ForAddress("https://localhost:50051");
+        //var client = new Gree
     }
 }
